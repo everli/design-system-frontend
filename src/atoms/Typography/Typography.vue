6c12ea7c7da34c352ec17caa9d401e88bdd234a4
pdrazewski <drazewski.przemyslaw@gmail.com>
2021-10-25 21:15:23 +0200

    atoms: add typo atom

---
+++ src/atoms/Typography/Typography.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div class="everli-typography">
+      <p class="t-large-title">Large Title</p>
+      <p class="t-title-1">Title 1</p>
+      <p class="t-title-2">Title 2</p>
+      <p class="t-title-3">Title 3</p>
+      <p class="t-headline">Headline</p>
+      <p class="t-body">Body</p>
+      <p class="t-subhead">Subhead</p>
+      <p class="t-footnote">Footnote</p>
+      <p class="t-caption">Caption</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "EverliTypography",
+}
+</script>
