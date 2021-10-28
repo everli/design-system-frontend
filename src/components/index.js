8dd9801025e15af7329cb3be24c6656d7ece3ff3
pdrazewski <drazewski.przemyslaw@gmail.com>
2021-10-28 15:36:03 +0200

    update builds

--- src/components/index.js
+++ src/components/index.js
@@ -1,16 +1,1 @@
-import Vue from "vue"
-
-const Components = {}
-
-const requireComponent = require.context("./", true, /[A-Z]\w+\.(vue|js)$/)
-
-requireComponent.keys().forEach((fileName) => {
-  const componentConfig = requireComponent(fileName)
-  const component = componentConfig.default || componentConfig
-
-  Components[component.name] = component
-
-  Vue.component(component.name, component)
-})
-
-export default Components
+export { default as EverliButton } from "./Button/Button.vue"
