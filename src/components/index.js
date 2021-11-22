636a22d7bcd17a27f3f44ef3e9776eded73f75de
pdrazewski <drazewski.przemyslaw@gmail.com>
2021-11-22 13:18:49 +0100

    feat: update build

--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,26 @@
-export { default as EverliButton } from "./Button/Button.vue"
+import Vue from "vue"
+import Button from "@/components/Button/Button.vue"
+
+const EverliDesignSystem = {
+  Button,
+}
+
+const requireComponent = require.context("./", true, /[A-Z]\w+\.(vue)$/)
+EverliDesignSystem.install = () =>
+  requireComponent.keys().forEach((fileName) => {
+    const componentConfig = requireComponent(fileName)
+    const component = componentConfig.default || componentConfig
+
+    EverliDesignSystem[component.name] = component
+
+    Vue.component(component.name, component)
+  })
+
+// Automatically install Keen UI if Vue is available globally
+if (typeof window !== "undefined" && window.Vue) {
+  window.Vue.use(EverliDesignSystem, window.EverliDesignSystemConfig)
+}
+
+export default EverliDesignSystem
+
+export { Button }
