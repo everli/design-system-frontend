c0cdd4f81fae7bbca0159fb213ceb26964f762cd
pdrazewski <drazewski.przemyslaw@gmail.com>
2021-11-22 13:46:05 +0100

    ping

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,21 @@
+import Vue from "vue"
 import EverliButton from "@/components/Button/Button.vue"
+
+const EverliDesignSystem = {
+  EverliButton,
+}
+
+const requireComponent = require.context("./", true, /[A-Z]\w+\.(vue)$/)
+EverliDesignSystem.install = () =>
+  requireComponent.keys().forEach((fileName) => {
+    const componentConfig = requireComponent(fileName)
+    const component = componentConfig.default || componentConfig
+
+    EverliDesignSystem[component.name] = component
+
+    Vue.component(component.name, component)
+  })
+
+export default EverliDesignSystem
 
 export { EverliButton }
