ce67faadea37d9acd6363595b88a8913e9df91a7
pdrazewski <drazewski.przemyslaw@gmail.com>
2021-11-23 21:04:07 +0100

    update button

--- src/components/Button/Button.vue
+++ src/components/Button/Button.vue
@@ -1,34 +1,15 @@
 <template>
-  <div>SLIDER SUCCO</div>
+  <div class="ev-button">SLIDER SUCCO</div>
 </template>
 
 <script>
 export default {
-  name: "slide",
-  props: ["title"],
-  data() {
-    return {
-      width: null,
-    }
-  },
+  name: "evButton",
 }
 </script>
 
 <style lang="scss">
-.VueCarousel-slide {
-  flex-basis: inherit;
-  flex-grow: 0;
-  flex-shrink: 0;
-  user-select: none;
-  backface-visibility: hidden;
-  -webkit-touch-callout: none;
-  -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
-  outline: none;
-}
-
-.VueCarousel-slide-adjustableHeight {
-  display: table;
-  flex-basis: auto;
-  width: 100%;
+.ev-button {
+  color: $c-green-100;
 }
 </style>
