4641c8055aeb22574cd051c648c9d33c548b3a08
pdrazewski <drazewski.przemyslaw@gmail.com>
2021-11-23 20:52:45 +0100

    up

--- src/components/Button/Button.vue
+++ src/components/Button/Button.vue
@@ -1,17 +1,5 @@
 <template>
-  <div
-    class="VueCarousel-slide"
-    tabindex="-1"
-    :aria-hidden="!isActive"
-    role="tabpanel"
-    :class="{
-      'VueCarousel-slide-active': isActive,
-      'VueCarousel-slide-center': isCenter,
-      'VueCarousel-slide-adjustableHeight': isAdjustableHeight,
-    }"
-  >
-    <slot></slot>
-  </div>
+  <div>SLIDER SUCCO</div>
 </template>
 
 <script>
@@ -22,64 +10,6 @@ export default {
     return {
       width: null,
     }
-  },
-  mounted() {
-    if (!this.$isServer) {
-      this.$el.addEventListener("dragstart", (e) => e.preventDefault())
-    }
-
-    this.$el.addEventListener(
-      this.carousel.isTouch ? "touchend" : "mouseup",
-      this.onTouchEnd
-    )
-  },
-  computed: {
-    activeSlides() {
-      return false
-    },
-    /**
-     * `isActive` describes whether a slide is visible
-     * @return {Boolean}
-     */
-    isActive() {
-      return this.activeSlides.indexOf(this._uid) >= 0
-    },
-    /**
-     * `isCenter` describes whether a slide is in the center of all visible slides
-     * if perPage is an even number, we quit
-     * @return {Boolean}
-     */
-    isCenter() {
-      const { breakpointSlidesPerPage } = this.carousel
-      if (breakpointSlidesPerPage % 2 === 0 || !this.isActive) return false
-      return (
-        this.activeSlides.indexOf(this._uid) ===
-        Math.floor(breakpointSlidesPerPage / 2)
-      )
-    },
-    /**
-     * `isAdjustableHeight` describes if the carousel adjusts its height to the active slide(s)
-     * @return {Boolean}
-     */
-    isAdjustableHeight() {
-      const { adjustableHeight } = this.carousel
-      return adjustableHeight
-    },
-  },
-  methods: {
-    onTouchEnd(e) {
-      /**
-       * @event slideclick
-       * @event slide-click
-       * @type {Object}
-       */
-      const eventPosX =
-        this.carousel.isTouch && e.changedTouches && e.changedTouches.length > 0
-          ? e.changedTouches[0].clientX
-          : e.clientX
-      const deltaX = this.carousel.dragStartX - eventPosX
-      return deltaX
-    },
   },
 }
 </script>
