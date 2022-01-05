170bb217c8eaa01b711e7e5b50e9248c88478275
pdrazewski <drazewski.przemyslaw@gmail.com>
2022-01-05 16:57:32 +0100

    feat: implement SVG setup with inline and external source

---
+++ scripts/icons.js
@@ -0,0 +1,38 @@
+const gulp = require("gulp")
+const svgSprite = require("gulp-svg-sprite")
+const wrap = require("gulp-wrap")
+const extname = require("gulp-extname")
+
+gulp.task("inline", function () {
+  return gulp
+    .src("../src/static/icons/*.svg")
+    .pipe(
+      svgSprite({
+        mode: {
+          symbol: {
+            inline: true,
+            example: false,
+          },
+        },
+      })
+    )
+    .pipe(wrap("<template>\n<%= contents %>\n</template>\r\n"))
+    .pipe(extname(".vue"))
+    .pipe(gulp.dest("../src/components/SvgSprite"))
+})
+
+gulp.task("external", function () {
+  return gulp
+    .src("../src/static/icons/*.svg")
+    .pipe(
+      svgSprite({
+        mode: {
+          symbol: {
+            inline: false,
+            example: false,
+          },
+        },
+      })
+    )
+    .pipe(gulp.dest("../static/icons/svg/sprite"))
+})
