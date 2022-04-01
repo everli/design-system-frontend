219f76bf3fcc4fdc659d9540fff883a94ca02201
pdrazewski <drazewski.przemyslaw@gmail.com>
2022-04-01 16:39:51 +0200

    feat: update icons pack

---
+++ src/atoms/Icons/IconsDemo.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <EverliSvgSprite />
+    <table>
+      <tr v-for="(icon, index) in icons" :key="index">
+        <td>
+          <svg :aria-label="icon">
+            <use :xlink:href="`#${icon}`"></use>
+          </svg>
+        </td>
+        <td>
+          {{ icon }}
+        </td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+import EverliSvgSprite from "../../components/Icons/symbol/svg/sprite.symbol.vue"
+import IconsList from "../../components/Icons/icons.json"
+
+export default {
+  components: { EverliSvgSprite },
+  computed: {
+    icons() {
+      const iconTransformed = IconsList.map((icon) =>
+        icon.split("icons/")[1].replace(".svg", "")
+      )
+
+      return iconTransformed
+    },
+  },
+}
+</script>
--- src/atoms/Icons/SvgIcons.stories.js
+++ src/atoms/Icons/SvgIcons.stories.js
@@ -1,80 +1,12 @@
-import EverliSvgSprite from "../../components/Icons/symbol/svg/sprite.symbol.vue"
+import IconsDemo from "./IconsDemo.vue"
 
 export default {
   title: "Icons/Icons",
 }
 
 export const Icons = () => ({
-  components: { EverliSvgSprite },
-  template: `<div style="display: flex; flex-wrap: wrap">
-        <EverliSvgSprite />
-        <svg style="width: 50px; margin: 5px"><use xlink:href="#ico-action-more"></use></svg>
-        <svg style="width: 50px; margin: 5px"><use xlink:href="#ico-add"></use></svg>
-        <svg style="width: 50px; margin: 5px"><use xlink:href="#ico-add-circle"></use></svg>
-        <svg style="width: 50px; margin: 5px"><use xlink:href="#ico-alert"></use></svg>
-        <svg style="width: 50px; margin: 5px"><use xlink:href="#ico-arrow-left"></use></svg>
-        <svg style="width: 50px; margin: 5px"><use xlink:href="#ico-arrow-left-circle"></use></svg>
-        <svg style="width: 50px; margin: 5px"><use xlink:href="#ico-arrow-right"></use></svg>
-        <svg style="width: 50px; margin: 5px"><use xlink:href="#ico-ask-question"></use></svg>
-        <svg style="width: 50px; margin: 5px"><use xlink:href="#ico-cart"></use></svg>
-        <svg style="width: 50px; margin: 5px"><use xlink:href="#ico-cash"></use></svg>
-        <svg style="width: 50px; margin: 5px"><use xlink:href="#ico-categories"></use></svg>
-        <svg style="width: 50px; margin: 5px"><use xlink:href="#ico-chat"></use></svg>
-        <svg style="width: 50px; margin: 5px"><use xlink:href="#ico-checkmark"></use></svg>
-        <svg style="width: 50px; margin: 5px"><use xlink:href="#ico-checkmark-circle-filled"></use></svg>
-        <svg style="width: 50px; margin: 5px"><use xlink:href="#ico-checkmark-circle-outline"></use></svg>
-        <svg style="width: 50px; margin: 5px"><use xlink:href="#ico-chevron-down"></use></svg>
-        <svg style="width: 50px; margin: 5px"><use xlink:href="#ico-chevron-left"></use></svg>
-        <svg style="width: 50px; margin: 5px"><use xlink:href="#ico-chevron-right"></use></svg>
-        <svg style="width: 50px; margin: 5px"><use xlink:href="#ico-chevron-top"></use></svg>
-        <svg style="width: 50px; margin: 5px"><use xlink:href="#ico-clock"></use></svg>
-        <svg style="width: 50px; margin: 5px"><use xlink:href="#ico-close"></use></svg>
-        <svg style="width: 50px; margin: 5px"><use xlink:href="#ico-contract"></use></svg>
-        <svg style="width: 50px; margin: 5px"><use xlink:href="#ico-cookie"></use></svg>
-        <svg style="width: 50px; margin: 5px"><use xlink:href="#ico-credit-card"></use></svg>
-        <svg style="width: 50px; margin: 5px"><use xlink:href="#ico-delete"></use></svg>
-        <svg style="width: 50px; margin: 5px"><use xlink:href="#ico-docs"></use></svg>
-        <svg style="width: 50px; margin: 5px"><use xlink:href="#ico-download"></use></svg>
-        <svg style="width: 50px; margin: 5px"><use xlink:href="#ico-edit"></use></svg>
-        <svg style="width: 50px; margin: 5px"><use xlink:href="#ico-email"></use></svg>
-        <svg style="width: 50px; margin: 5px"><use xlink:href="#ico-empty"></use></svg>
-        <svg style="width: 50px; margin: 5px"><use xlink:href="#ico-error"></use></svg>
-        <svg style="width: 50px; margin: 5px"><use xlink:href="#ico-favorite-filled"></use></svg>
-        <svg style="width: 50px; margin: 5px"><use xlink:href="#ico-favorite-outline"></use></svg>
-        <svg style="width: 50px; margin: 5px"><use xlink:href="#ico-filters"></use></svg>
-        <svg style="width: 50px; margin: 5px"><use xlink:href="#ico-filters-multiple"></use></svg>
-        <svg style="width: 50px; margin: 5px"><use xlink:href="#ico-flash"></use></svg>
-        <svg style="width: 50px; margin: 5px"><use xlink:href="#ico-flash-circle"></use></svg>
-        <svg style="width: 50px; margin: 5px"><use xlink:href="#ico-forward"></use></svg>
-        <svg style="width: 50px; margin: 5px"><use xlink:href="#ico-gift"></use></svg>
-        <svg style="width: 50px; margin: 5px"><use xlink:href="#ico-gift-card"></use></svg>
-        <svg style="width: 50px; margin: 5px"><use xlink:href="#ico-gps"></use></svg>
-        <svg style="width: 50px; margin: 5px"><use xlink:href="#ico-help"></use></svg>
-        <svg style="width: 50px; margin: 5px"><use xlink:href="#ico-home"></use></svg>
-        <svg style="width: 50px; margin: 5px"><use xlink:href="#ico-info"></use></svg>
-        <svg style="width: 50px; margin: 5px"><use xlink:href="#ico-invoice"></use></svg>
-        <svg style="width: 50px; margin: 5px"><use xlink:href="#ico-menu"></use></svg>
-        <svg style="width: 50px; margin: 5px"><use xlink:href="#ico-note-product"></use></svg>
-        <svg style="width: 50px; margin: 5px"><use xlink:href="#ico-note-shopper"></use></svg>
-        <svg style="width: 50px; margin: 5px"><use xlink:href="#ico-notifications"></use></svg>
-        <svg style="width: 50px; margin: 5px"><use xlink:href="#ico-padlock"></use></svg>
-        <svg style="width: 50px; margin: 5px"><use xlink:href="#ico-pos"></use></svg>
-        <svg style="width: 50px; margin: 5px"><use xlink:href="#ico-position-marker-filled"></use></svg>
-        <svg style="width: 50px; margin: 5px"><use xlink:href="#ico-position-marker-outline"></use></svg>
-        <svg style="width: 50px; margin: 5px"><use xlink:href="#ico-question-mark"></use></svg>
-        <svg style="width: 50px; margin: 5px"><use xlink:href="#ico-remove"></use></svg>
-        <svg style="width: 50px; margin: 5px"><use xlink:href="#ico-replacements"></use></svg>
-        <svg style="width: 50px; margin: 5px"><use xlink:href="#ico-save"></use></svg>
-        <svg style="width: 50px; margin: 5px"><use xlink:href="#ico-search"></use></svg>
-        <svg style="width: 50px; margin: 5px"><use xlink:href="#ico-settings"></use></svg>
-        <svg style="width: 50px; margin: 5px"><use xlink:href="#ico-shield-consents"></use></svg>
-        <svg style="width: 50px; margin: 5px"><use xlink:href="#ico-shield-privacy"></use></svg>
-        <svg style="width: 50px; margin: 5px"><use xlink:href="#ico-shopping-list"></use></svg>
-        <svg style="width: 50px; margin: 5px"><use xlink:href="#ico-user"></use></svg>
-        <svg style="width: 50px; margin: 5px"><use xlink:href="#ico-user-verified"></use></svg>
-        <svg style="width: 50px; margin: 5px"><use xlink:href="#ico-wallet"></use></svg>
-        <svg style="width: 50px; margin: 5px"><use xlink:href="#ico-zoom"></use></svg>
-    </div> `,
+  components: { IconsDemo },
+  template: "<IconsDemo />",
 })
 
 Icons.story = {
